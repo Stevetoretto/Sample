cb07c9bdd4145be60ac55a3b89b21eca68f3625a
Steve <stephenjoseph1111@gmail.com>
2025-05-13 15:49:29 +0530

    Updated Automation Scripts

--- src/test/java/org/demo/Booking.java
+++ src/test/java/org/demo/Booking.java
@@ -1,19 +1,35 @@
 package org.demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Booking {
 	
-	public void book() {
+	WebDriver driver;
+	
+	public void websiteNavigation() {
 	WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver();
-    driver.get("");
+    driver = new ChromeDriver();
+    driver.get("https://testautomationpractice.blogspot.com/");
+    driver.manage().window().maximize();
+    System.out.println("User Navigated to : " + driver.getCurrentUrl());
+	}
+	
+	public void userDetails(){
+	driver.findElement(By.xpath("//input[@id='name']")).sendKeys("QA Automation");
+	driver.findElement(By.xpath("//input[@id='email']")).sendKeys("XYZ@yopmail.com");
+	System.out.println("User Details has been Updated");
 	}
 
 	public static void main(String[] args) {
+		Booking b = new Booking();
+		b.websiteNavigation();
+		b.userDetails();
 		
 	}
 
